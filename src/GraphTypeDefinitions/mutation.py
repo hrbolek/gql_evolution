9daeb416a1ad6c117ee55c3fb1fa2ad237e70d24
vojtechvel <vojtechvel@gmail.com>
2025-12-14 17:52:30 +0100

    pridani_insert_pro_fin/mill

--- src/GraphTypeDefinitions/mutation.py
+++ src/GraphTypeDefinitions/mutation.py
@@ -4,8 +4,10 @@ import strawberry
 from .EventGQLModel import EventMutation
 from .EventInvitationGQLModel import EventInvitationMutation
 from .ProjectGQLModel import ProjectMutation
+from .FinanceGQLModel import FinanceMutation
+from .MilestoneGQLModel import MilestoneMutation
 
 @strawberry.type(description="""Type for mutation root""")
-class Mutation(EventMutation, EventInvitationMutation, ProjectMutation):
+class Mutation(EventMutation, EventInvitationMutation, ProjectMutation, FinanceMutation, MilestoneMutation):
     pass
 
